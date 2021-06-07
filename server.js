de74bebb39c819afd9423a2b71bf169f3808bbee
Jordan Masone <jordanmasone1@gmail.com>
2021-06-07 02:15:34 -0400

    Added login system.

--- server.js
+++ server.js
@@ -12,12 +12,24 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("tiny"));
 
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
 // Database
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/chatApp", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}, (err) => {
+    if (err) throw err;
+    console.log("Connected to MongoDB");
+});
 
 // Routes
-app.get("/", (req, res) => {
-    res.send("Hello, world!");
-});
+app.use("/user", require("./routes/userRoutes"));
 
 
 // Start Server
